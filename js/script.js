561bb663284ca872bed4c30ae15996339c374112
danielevergani <dvergani94@gmail.com>
2021-03-26 15:22:57 +0100

    aggiungere elementi da input con click su bottone

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ var app = new Vue(
     {
        el: "#root" ,
        data: {
-           elementi: ["latte", "uova", "pane"]
-       }
+           elementi: ["latte", "uova", "pane"],
+           newItem: ""
+       },
+       methods:{
+        removeElem: function(i){
+            this.elementi.splice(i, 1)
+        },
+        addElem: function(){
+            if (this.newItem != 0){
+                this.elementi.push(this.newItem);
+                this.newItem = "";
+            }
+            
+        }
+    }
     }
 );
